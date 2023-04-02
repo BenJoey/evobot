4d4ece52f7889cf47c176fcbeb4e7cd88c5d3408
Bence <bauerbence5@gmail.com>
2023-04-02 11:02:35 +0200

    Removed redundant file

--- discordjs_patch.py
+++
@@ -1,8 +0,0 @@
-with open("node_modules/@discordjs/voice/dist/index.js", "r") as f:
-    contents = f.readlines()
-
-contents.insert(1361, "\t\tthis.configureNetworking();\n")
-
-with open("node_modules/@discordjs/voice/dist/index.js", "w") as f:
-    contents = "".join(contents)
-    f.write(contents)
