c4375a6016be6dc3b8c894cea53bf258bb77cc39
Bence <bauerbence5@gmail.com>
2023-09-08 16:48:40 +0200

    Temp fix for another bug caused by third party libraries

--- utils/errorMessage.ts
+++ utils/errorMessage.ts
@@ -1,14 +1,14 @@
 import { i18n } from "../utils/i18n";
 
 export function getErrorMessage(error:any, extra: any = undefined): string {
-    try {
-      switch(error.statusCode) {
-        case 410: 
-            return extra == undefined ? i18n.__mf("common.ageRestrictionNoSong") : i18n.__mf("common.ageRestriction", { song: extra });
-        default: return i18n.__mf("common.errorCommandUnknown", { err: error });
-      }
-    } catch (e) {
-      console.error(e);
-      return i18n.__("common.errorCommand");
+  try {
+    switch(error.statusCode) {
+      case 410: 
+          return extra == undefined ? i18n.__mf("common.ageRestrictionNoSong") : i18n.__mf("common.ageRestriction", { song: extra });
+      default: return i18n.__mf("common.errorCommandUnknown", { err: error });
     }
+  } catch (e) {
+    console.error(e);
+    return i18n.__("common.errorCommand");
+  }
 }
